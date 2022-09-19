e4a5ee2c54899002a108519d58d6c365612823c3
Filip <filiptankiewicz04@gmail.com>
2022-09-20 00:45:14 +0200

    idea of card flip changed

--- src/components/CardFlip/index.tsx
+++ src/components/CardFlip/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, FC } from "react";
 import { SafeAreaView, StyleSheet, View, Text } from "react-native";
-import { IconButton, MD3Colors, Button } from "react-native-paper";
+import { IconButton, MD3Colors, Button, Snackbar } from "react-native-paper";
 import { CARD_DATA } from "../CardData";
 import { DM_WIDTH, DM_HEIGHT } from "../../common/Dimensions";
+import ModalData from "../../helpers/getModalData";
 
 const CardFlip = ({ navigation }: any) => {
   const randomCard = CARD_DATA[Math.floor(Math.random() * CARD_DATA.length)];
@@ -17,25 +18,45 @@ const CardFlip = ({ navigation }: any) => {
     };
   }, [setImage]);
 
+
+  const [visible, setVisible] = React.useState(false);
+
+  const onToggleSnackBar = () => setVisible(!visible);
+
+  const onDismissSnackBar = () => setVisible(false);
+
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {image && (
-        <View style={styles.image}>
-          <View style={styles.containerArrow}>
-            <IconButton
-              icon="arrow-left"
-              iconColor={MD3Colors.error50}
-              size={34}
-              onPress={() => navigation.navigate("Home")}
-            />
-          </View>
-          <View style={styles.imageAnimate}>
-            <Text style={{ color: "red", fontSize: 24 }}></Text>
-            <Text style={{ color: "red", fontSize: 24 }}></Text>
-            <Button onPress={() => console.log("pressed")}>Klik</Button>
-          </View>
+      <View style={styles.image}>
+        <View style={styles.containerArrow}>
+          <IconButton
+            icon="arrow-left"
+            iconColor={MD3Colors.error50}
+            size={34}
+            onPress={() => navigation.navigate("Home")}
+          />
         </View>
-      )}
+        <View style={styles.imageAnimate}>
+          <Text style={{ color: "red", fontSize: 24 }}></Text>
+          <Text style={{ color: "red", fontSize: 24 }}></Text>
+          <Button onPress={onToggleSnackBar}>{visible ? 'Hide' : 'Show'}</Button>
+      <Snackbar
+        style={styles.imageAnimateSnack}
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        duration={Infinity}
+        action={{
+          label: 'Undo',
+          onPress: () => {
+            navigation.navigate("Information")
+          },
+        }}>
+       <ModalData />
+      </Snackbar>
+      <Button onPress={onToggleSnackBar}>{visible ? 'Hide' : null}</Button>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -63,7 +84,24 @@ const styles = StyleSheet.create({
     backgroundColor: "green",
     width: DM_WIDTH - 50,
     height: DM_HEIGHT / 1.5,
-    margin: "5.8%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "flex-start",
     borderRadius: 20,
+    top: "10%",
+    margin: "6.5%",
   },
+  imageAnimateSnack: {
+    backgroundColor: "gray",
+    width: DM_WIDTH - 50,
+    height: DM_HEIGHT / 1.5,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    borderRadius: 20,
+    top: "0%",
+    margin: "0%",
+  }
 });
