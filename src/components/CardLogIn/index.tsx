849fecad4ba5d4a2664a2c58a08391738cfb9717
Filip <filiptankiewicz04@gmail.com>
2022-09-27 00:16:14 +0200

    static errors on login card added

--- src/components/CardLogIn/index.tsx
+++ src/components/CardLogIn/index.tsx
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
 import { View } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import { TextInput, Button, Text } from "react-native-paper";
 //@ts-ignore
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 export default function CardLogIn({ navigation }: NativeStackScreenProps) {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState({});
 
   //   console.log("Nazwa:", name);
   //   console.log("Hasło:", password);
 
+  const validNameWithPassword = name === "Geralt" && password === "Ciri";
+  const inValidPassword = name === "Geralt" && password != "Ciri";
+  const inValidName = name != "Geralt" && password === "Ciri";
+  const inValidNameWithPassword = name != "Geralt" && password != "Ciri";
+
+  const badName = name != "" && name != "Geralt";
+  const badPassword = password != "" && password != "Ciri";
+
   const checkData = () => {
-    if (name === "Geralt" && password === "Ciri") {
+    if (validNameWithPassword) {
       return navigation.navigate("Home");
-    } else if (name === "Geralt" && password != "Ciri") {
+    } else if (inValidPassword) {
       console.log("Poprawna nazwa, hasło nie");
-    } else if (name != "Geralt" && password === "Ciri") {
+    } else if (inValidName) {
       console.log("Poprawne hasło, nazwa nie");
-    } else if (name != "Geralt" && password != "Ciri") {
+    } else if (inValidNameWithPassword) {
       console.log("Nie poprawne dane");
     }
   };
@@ -30,14 +39,22 @@ export default function CardLogIn({ navigation }: NativeStackScreenProps) {
         value={name}
         onChangeText={(name) => setName(name)}
         style={{ top: 20, width: "80%" }}
+        error={badName}
       />
+      <View style={{ top: 30 }}>
+        {badName && <Text style={{ color: "red" }}>Zła nazwa</Text>}
+      </View>
       <TextInput
         label="Hasło"
         secureTextEntry
         value={password}
         onChangeText={(password) => setPassword(password)}
         style={{ top: 40, width: "80%" }}
+        error={badPassword}
       />
+      <View style={{ top: 50 }}>
+        {badPassword && <Text style={{ color: "red" }}>Złe hasło</Text>}
+      </View>
       <Button
         style={{ top: 60, width: "80%" }}
         icon="camera"
